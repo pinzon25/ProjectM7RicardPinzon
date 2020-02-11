73e456bdd1dbbf3691af8af20eec72f8bf32068a
Ricard <ricardpinzonsuller@gmail.com>
2020-02-11 19:39:08 +0100

    pujada de projecte

---
+++ app/src/main/java/com/example/projectem7ricardpinzon/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.projectem7ricardpinzon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etusername;
+    EditText etpassword;
+    Button login;
+    Button registre;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        etusername = findViewById(R.id.txt_User);
+        etpassword = findViewById(R.id.txt_Password);
+        login = findViewById(R.id.btn_signup);
+        registre = findViewById(R.id.btn_registre);
+
+    }
+    public void formulariRegistre(View view) {
+        Intent intent = new Intent(this, Registre.class);
+
+        startActivity(intent);
+
+
+
+    }
+    public void login(View view) {
+
+        final String username = etusername.getText().toString();
+        final String password = etpassword.getText().toString();
+
+        if(username.equals("user") && password.equals("user")){
+
+            saveLoginSharedPreferences(username);
+
+            Intent intent = new Intent(this, principal.class);
+
+            startActivity(intent);
+
+        }else{
+            Toast toast =
+                    Toast.makeText(getApplicationContext(),
+                            "Usuari o contrasenya incorrectes.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER|Gravity.LEFT,0,0);
+            toast.show();
+        }
+
+
+
+    }
+    private void saveLoginSharedPreferences(String username){
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("username", username);//guardar los datos
+        editor.commit();
+        editor.apply();
+
+    }
+    public void cambiaCat(View view) {
+        Locale catalan = new Locale("ca", "CA");
+        Locale.setDefault(catalan);
+        Configuration config = new Configuration();
+        config.locale = catalan;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent cat = new Intent(getApplicationContext(), MainActivity.class);
+        cat.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(cat);
+    }
+    public void cambiaEsp(View view) {
+        Locale espanol = new Locale("es", "ES");
+        Locale.setDefault(espanol);
+        Configuration config = new Configuration();
+        config.locale = espanol;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent esp = new Intent(getApplicationContext(), MainActivity.class);
+        esp.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(esp);
+    }
+    public void cambiaEn(View view) {
+        Locale ingles = new Locale("en", "ES");
+        Locale.setDefault(ingles);
+        Configuration config = new Configuration();
+        config.locale = ingles;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent ing = new Intent(getApplicationContext(), MainActivity.class);
+        ing.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(ing);
+    }
+
+}
---
+++ app/src/main/java/com/example/projectem7ricardpinzon/Registre.java
@@ -0,0 +1,70 @@
+package com.example.projectem7ricardpinzon;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registre extends AppCompatActivity {
+
+    EditText nom;
+    EditText cognom;
+    EditText username;
+    EditText contrasenya;
+    RadioButton home;
+    RadioButton dona;
+    SeekBar seekBar;
+    Spinner sp_frase;
+    CheckBox checkBox;
+    Button btn_registrar;
+    SharedPreferences prefs ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registre);
+
+        prefs  = getSharedPreferences("Usuarios", Context.MODE_PRIVATE);
+    }
+
+    public void registrar(View view) {
+
+        nom = findViewById(R.id.txt_nom);
+        cognom = findViewById(R.id.txt_cognom);
+        username = findViewById(R.id.txt_username);
+        contrasenya = findViewById(R.id.txt_contrasenya);
+        home = findViewById(R.id.rb_home);
+        dona= findViewById(R.id.rb_dona);
+        seekBar= findViewById(R.id.seekBar);
+        sp_frase= findViewById(R.id.sp_frase);
+        checkBox= findViewById(R.id.checkBox);
+
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("nom", String.valueOf(nom));
+        editor.putString("cognom", String.valueOf(cognom));
+        editor.putString("user", String.valueOf(username));
+        editor.putString("password", String.valueOf(contrasenya));
+      /*  editor.putBoolean("home", Boolean.valueOf(home));
+        editor.putBoolean("dona", Boolean.valueOf(dona));
+        editor.putBoolean("seekBar", Boolean.valueOf(seekBar));
+        editor.putBoolean("sp_frase", Boolean.valueOf(sp_frase));
+        editor.putBoolean("checkBox", Boolean.valueOf(checkBox));*/
+        editor.commit();
+
+    }
+
+}
+
